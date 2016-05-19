bb305cf048869837935a4643d3e2799e54d27968
DanBalalan <danbalalan@gmail.com>
2016-05-19 15:09:09 +0500

    Подправил типы и формулы

--- Bank/calculations.cpp
+++ Bank/calculations.cpp
@@ -5,22 +5,22 @@
 //capitalization - срок начисления процентов (в месяцах);
 
 //Простые проценты (без капитализации)
-unsigned __int64 simple_deposit(unsigned int deposit, unsigned __int16 term, float percent){
+double simple_deposit(double deposit, unsigned __int16 term, double percent){
 	unsigned __int64 money = 0;
 	money = deposit * (1 + term*percent / (100 * 365));
 	return money;
 }
 //Сложные проценты (с капитализацией)
-unsigned __int64 capitalized_deposit(unsigned int deposit, unsigned __int16 term, float percent, unsigned __int8 capitalization, unsigned __int8 replenishment_period, unsigned int replenishment_amount){
-	unsigned __int64 money = 0;
-	unsigned int num_of_replenishment = term / replenishment_period;; //количество пополнений втечение срока
-	money = deposit * pow(1 + capitalization * 30 * percent / (100 * 365), num_of_replenishment);
+double capitalized_deposit(double deposit, unsigned __int16 term, double percent, unsigned __int8 capitalization){
+	double money = 0;
+	unsigned int num_of_cap = unsigned int(term / capitalization); //количество периодов капитализации
+	money = deposit * pow(1 + capitalization * 30 * percent / (100 * 365), num_of_cap);
 	return money;
 }
 
-unsigned __int64 count_my_money(unsigned int deposit, unsigned __int16 term, float percent, unsigned __int8 capitalization)
+double count_my_money(double deposit, unsigned __int16 term, double percent, unsigned __int8 capitalization)
 {
-	unsigned __int64 money=0; 
+	double money = 0;
 	if (capitalization = 0)
 		money = simple_deposit(deposit,term,percent);
 	else money = capitalized_deposit(deposit,term,percent,capitalization);
